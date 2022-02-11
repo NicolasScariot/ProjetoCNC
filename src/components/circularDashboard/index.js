d143dc0a63fbacefd000ff38235e128797e8f8ec
NicolasScariot <88735290+NicolasScariot@users.noreply.github.com>
2022-02-11 17:20:14 -0300

    Gráficos na página

--- src/components/circularDashboard/index.js
+++ src/components/circularDashboard/index.js
@@ -5,31 +5,41 @@ import 'react-circular-progressbar/dist/styles.css';
 // import AnimatedProgressProvider from "./AnimatedProgressProvider";
 // import ChangingProgressProvider from "./ChangingProgressProvider";
 
-const value = 0.66;
+const valueRpm = 50.00;
+const value = 70.00;
 
 <CircularProgressbar value={value} maxValue={1} text={`${value * 100}%`} />;
 
 
-const dashboards = () => {
+const Dashboards = () => {
   return (
     <div style={{ width: 200, height: 200 }}>
-      <Example label="Stroke width">
+      <Example label="RPM">
+      <CircularProgressbar
+        value={valueRpm}
+        text={`${valueRpm}%`}
+        strokeWidth={5}
+      />
+      </Example>
+      <Example label="Velocidade de avanço">
       <CircularProgressbar
         value={value}
         text={`${value}%`}
         strokeWidth={5}
       />
       </Example>
+
     </div>
+    
   );
 }
-export default dashboards;
+export default Dashboards;
 function Example(props) {
   return (
     <div style={{ marginBottom: 80 }}>
       <hr style={{ border: "2px solid #ddd" }} />
       <div style={{ marginTop: 30, display: "flex" }}>
-        <div style={{ width: "30%", paddingRight: 30 }}>{props.children}</div>
+        <div style={{ width: "650%", paddingRight: 30 }}>{props.children}</div>
         <div style={{ width: "70%" }}>
           <h3 className="h5">{props.label}</h3>
           <p>{props.description}</p>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header  from './components/header/index.js';
+import Dashboards from './components/circularDashboard/index.js';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header></Header>
+    <Dashboards></Dashboards>
   </React.StrictMode>,
   document.getElementById('root')
 );
